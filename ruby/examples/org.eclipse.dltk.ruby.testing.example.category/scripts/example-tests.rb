3dacf07c1443914a262e9e022238f1c07307972e
apanchenk <apanchenk>
2008-09-19 12:21:39 +0000

    example tests

---
+++ ruby/examples/org.eclipse.dltk.ruby.testing.example.category/scripts/example-tests.rb
@@ -0,0 +1,76 @@
+require 'test/unit'
+
+class AppleTests < Test::Unit::TestCase
+	def test1
+
+	end
+
+	def test2
+
+	end
+
+end
+
+class BigAppleTests < Test::Unit::TestCase
+    def test1
+
+    end
+
+    def test2
+
+    end
+
+end
+
+class OrangeTest1 < Test::Unit::TestCase
+	def test3
+
+	end
+
+	def test4
+
+	end
+end
+
+class OrangeTest2 < Test::Unit::TestCase
+	def test5
+
+	end
+
+	def test6
+
+	end
+end
+
+class ATests < Test::Unit::TestCase
+	def test7
+
+	end
+
+	def test8
+
+	end
+
+end
+
+class BTests < Test::Unit::TestCase
+    def test7
+
+    end
+
+    def test8
+
+    end
+
+end
+
+class CTests < Test::Unit::TestCase
+    def test7
+
+    end
+
+    def test8
+
+    end
+
+end
