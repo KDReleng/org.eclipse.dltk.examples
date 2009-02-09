9a54a15c628b2078f3c9d8e94a1c0a1d5083dc25
apanchenk <apanchenk>
2009-02-09 05:31:08 +0000

    updated signature of parseSourceModule() method

--- eclipsecon08/org.eclipse.dltk.examples.python/src/org/eclipse/dltk/examples/internal/python/core/parser/ExamplePythonSourceElementParser.java
+++ eclipsecon08/org.eclipse.dltk.examples.python/src/org/eclipse/dltk/examples/internal/python/core/parser/ExamplePythonSourceElementParser.java
@@ -6,19 +6,21 @@ import org.eclipse.dltk.core.AbstractSourceElementParser;
 import org.eclipse.dltk.core.ISourceModuleInfoCache.ISourceModuleInfo;
 import org.eclipse.dltk.examples.internal.python.core.ExamplePythonNature;
 
-public class ExamplePythonSourceElementParser extends AbstractSourceElementParser {
-	public void parseSourceModule(char[] contents,
-			ISourceModuleInfo astCache, char[] filename) {
+public class ExamplePythonSourceElementParser extends
+		AbstractSourceElementParser {
+	public void parseSourceModule(
+			org.eclipse.dltk.compiler.env.ISourceModule module,
+			ISourceModuleInfo astCache) {
 		ISourceElementRequestor requestor = getRequestor();
-		
+
 		requestor.enterModule();
 		TypeInfo info = new TypeInfo();
-		info.name = "Example type";
+		info.name = "Example type"; //$NON-NLS-1$
 		requestor.enterType(info);
 		requestor.exitType(0);
 		requestor.exitModule(0);
 	}
-	
+
 	protected String getNatureId() {
 		return ExamplePythonNature.PYTHON_NATURE;
 	}
