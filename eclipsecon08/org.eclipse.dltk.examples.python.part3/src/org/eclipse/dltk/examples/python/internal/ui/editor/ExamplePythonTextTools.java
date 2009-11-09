b8dc71b8fbda437e6e68784322ea64d1922ec20c
apanchenk <apanchenk>
2009-11-09 12:24:31 +0000

    formatting

--- eclipsecon08/org.eclipse.dltk.examples.python.part3/src/org/eclipse/dltk/examples/python/internal/ui/editor/ExamplePythonTextTools.java
+++ eclipsecon08/org.eclipse.dltk.examples.python.part3/src/org/eclipse/dltk/examples/python/internal/ui/editor/ExamplePythonTextTools.java
@@ -26,6 +26,7 @@ public class ExamplePythonTextTools extends ScriptTextTools {
 		return new ExamplePythonSourceViewerConfiguration(getColorManager(),
 				preferenceStore, editor, partitioning);
 	}
+
 	public IPartitionTokenScanner getPartitionScanner() {
 		return fPartitionScanner;
 	}
