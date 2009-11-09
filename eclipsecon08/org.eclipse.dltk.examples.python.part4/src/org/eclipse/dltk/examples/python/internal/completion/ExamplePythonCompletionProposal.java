08ce274909cc462c5bd11675bbdc8d456b9d28bb
apanchenk <apanchenk>
2009-11-09 17:43:44 +0000

    formatting

--- eclipsecon08/org.eclipse.dltk.examples.python.part4/src/org/eclipse/dltk/examples/python/internal/completion/ExamplePythonCompletionProposal.java
+++ eclipsecon08/org.eclipse.dltk.examples.python.part4/src/org/eclipse/dltk/examples/python/internal/completion/ExamplePythonCompletionProposal.java
@@ -13,18 +13,21 @@ public class ExamplePythonCompletionProposal extends ScriptCompletionProposal {
 		super(replacementString, replacementOffset, replacementLength, image,
 				displayString, relevance);
 	}
+
 	public ExamplePythonCompletionProposal(String replacementString,
 			int replacementOffset, int replacementLength, Image image,
 			String displayString, int relevance, boolean isInDoc) {
 		super(replacementString, replacementOffset, replacementLength, image,
 				displayString, relevance, isInDoc);
 	}
+
 	protected boolean isSmartTrigger(char trigger) {
 		if (trigger == '.') {
 			return true;
 		}
 		return false;
 	}
+
 	protected boolean insertCompletion() {
 		IPreferenceStore preference = PythonCorePlugin.getDefault()
 				.getPreferenceStore();
